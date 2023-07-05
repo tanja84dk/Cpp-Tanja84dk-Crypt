fb38f76b8c306d8c11bfdbd13a02edfdf179958b
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-05 16:13:07 +0000

    removed my tools library since arent used

--- cmake/Findtanja84dk_tools.cmake
+++
@@ -1,6 +0,0 @@
-CPMAddPackage(
-    NAME            tanja84dk_tools
-    GIT_REPOSITORY  https://github.com/tanja84dk/Cpp-Tanja84dk-Tools
-    VERSION         0.1.1
-    GIT_TAG         83930f484458926e962d3ed67da4d5d1e85b8c9c
-)
