a3b43c2089ab510860133d3613d606853764d973
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-05 16:13:57 +0000

    updated

--- include/Tanja84dk/crypt/crypt.h
+++ include/Tanja84dk/crypt/crypt.h
@@ -1,7 +1,9 @@
 #ifndef TANJA84DK_CRYPT_CRYPT_H
 #define TANJA84DK_CRYPT_CRYPT_H
+
 #include "base32/base32.h"
 #include "base64/base64.h"
 #include "binary/binary.h"
+#include "ceasar/ceasar.h"
 
 #endif // TANJA84DK_CRYPT_CRYPT_H
