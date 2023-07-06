2015aa2111f1d82bd9a65f9b10e2123f386ea97b
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-06 20:05:13 +0000

    Initial ceasar chipher

--- include/Tanja84dk/crypt/ceasar/ceasar.h
+++ include/Tanja84dk/crypt/ceasar/ceasar.h
@@ -1,9 +1,12 @@
 #ifndef TANJA84DK_CRYPT_CEASAR_CEASAR_H
 #define TANJA84DK_CRYPT_CEASAR_CEASAR_H
+#include <string>
 
 namespace Tanja84dk::crypt::ceasar
 {
-
+    std::string encode(const int chipher_shift_key, const std::string &input_data);
+    std::string decode(const int chipher_shift_key, const std::string &input_data);
+    void decode_brute_force(const std::string &input_data);
 }
 
 #endif // TANJA84DK_CRYPT_CEASAR_CEASAR_h
