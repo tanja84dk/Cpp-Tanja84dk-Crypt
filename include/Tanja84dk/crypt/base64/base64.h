be5471f4f34fd534ada1436363e920ff7a394404
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-06 20:04:53 +0000

    test for file handler

--- include/Tanja84dk/crypt/base64/base64.h
+++ include/Tanja84dk/crypt/base64/base64.h
@@ -1,11 +1,15 @@
 #ifndef TANJA84DK_CRYPT_BASE64_BASE64_H
 #define TANJA84DK_CRYPT_BASE64_BASE64_H
 
+#include <iostream>
+#include <fstream>
 #include <string>
 
 namespace Tanja84dk::crypt::base64
 {
-    std::string decode(const std::string &input_data);
-    std::string encode(const std::string &input_data);
+    std::string decode(const std::string &input_data) noexcept;
+    std::string encode(const std::string &input_data) noexcept;
+    std::ofstream decode(const std::ifstream &input_file_handler) noexcept;
+    std::ofstream encode(const std::ifstream &input_file_handler) noexcept;
 }
 #endif // TANJA84DK_CRYPT_BASE64_BASE64_h
--- src/base64/base64.cpp
+++ src/base64/base64.cpp
@@ -1,5 +1,6 @@
 #include <Tanja84dk/crypt/base64/base64.h>
 #include <string>
+#include <sstream>
 
 #include <base64.h>
 
@@ -11,4 +12,32 @@ std::string Tanja84dk::crypt::base64::decode(const std::string &input_data)
 std::string Tanja84dk::crypt::base64::encode(const std::string &input_data)
 {
     return base64_encode(input_data);
+}
+
+std::ofstream Tanja84dk::crypt::base64::decode(const std::ifstream &input_file_handler) noexcept
+{
+    std::stringstream buffer_input_sstream = {};
+    buffer_input_sstream.clear();
+    std::ofstream output_file_handler;
+    while (input_file_handler.good())
+    {
+        buffer_input_sstream << input_file_handler.rdbuf();
+    }
+    base64_decode(buffer_input_sstream.str());
+
+    return output_file_handler;
+}
+
+std::ofstream Tanja84dk::crypt::base64::encode(const std::ifstream &input_file_handler) noexcept
+{
+    std::stringstream buffer_input_sstream = {};
+    buffer_input_sstream.clear();
+    std::ofstream output_file_handler;
+    while (input_file_handler.good())
+    {
+        buffer_input_sstream << input_file_handler.rdbuf();
+    }
+    base64_decode(buffer_input_sstream.str());
+
+    return output_file_handler;
 }
