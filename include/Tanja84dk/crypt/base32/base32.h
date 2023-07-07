a478f2879af0ad523d66f460fd3f6d5574f1f533
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-07 02:08:09 +0000

    initial base32

--- include/Tanja84dk/crypt/base32/base32.h
+++ include/Tanja84dk/crypt/base32/base32.h
@@ -1,4 +1,17 @@
 #ifndef TANJA84DK_CRYPT_BASE32_BASE32_H
 #define TANJA84DK_CRYPT_BASE32_BASE32_H
+#include <string>
+
+namespace Tanja84dk::crypt::base32
+{
+    std::string decode(const std::string &input_data) noexcept;
+    std::string encode(const std::string &input_data) noexcept;
+}
+
+namespace Tanja84dk::crypt::base32hex
+{
+    std::string decode(const std::string &input_data) noexcept;
+    std::string encode(const std::string &input_data) noexcept;
+}
 
 #endif // TANJA84DK_CRYPT_BASE32_BASE32_h
--- src/base32/base32.cpp
+++ src/base32/base32.cpp
@@ -1,1 +1,72 @@
 #include <Tanja84dk/crypt/base32/base32.h>
+#include <string>
+
+#include <base32.h>
+
+std::string Tanja84dk::crypt::base32::decode(const std::string &input_data) noexcept
+{
+    CryptoPP::Base32Decoder Decoder;
+    std::string decoded_string;
+    Decoder.Put((CryptoPP::byte *)input_data.data(), input_data.size());
+    Decoder.MessageEnd();
+
+    CryptoPP::word64 size_word64 = Decoder.MaxRetrievable();
+
+    if (size_word64 && size_word64 <= SIZE_MAX)
+    {
+        decoded_string.resize(size_word64);
+        Decoder.Get((CryptoPP::byte *)&decoded_string[0], decoded_string.size());
+    }
+    return decoded_string;
+}
+
+std::string Tanja84dk::crypt::base32::encode(const std::string &input_data) noexcept
+{
+    CryptoPP::Base32Encoder Encoder;
+    std::string encoded_string = {};
+    Encoder.Put((CryptoPP::byte *)input_data.data(), input_data.size());
+    Encoder.MessageEnd();
+
+    CryptoPP::word64 size_word64 = Encoder.MaxRetrievable();
+    if (size_word64)
+    {
+        encoded_string.resize(size_word64);
+        Encoder.Get((CryptoPP::byte *)&encoded_string[0], encoded_string.size());
+    }
+
+    return encoded_string;
+}
+
+std::string Tanja84dk::crypt::base32hex::decode(const std::string &input_data) noexcept
+{
+    CryptoPP::Base32HexDecoder Decoder;
+    std::string decoded_string;
+    Decoder.Put((CryptoPP::byte *)input_data.data(), input_data.size());
+    Decoder.MessageEnd();
+
+    CryptoPP::word64 size_word64 = Decoder.MaxRetrievable();
+
+    if (size_word64 && size_word64 <= SIZE_MAX)
+    {
+        decoded_string.resize(size_word64);
+        Decoder.Get((CryptoPP::byte *)&decoded_string[0], decoded_string.size());
+    }
+    return decoded_string;
+}
+
+std::string Tanja84dk::crypt::base32hex::encode(const std::string &input_data) noexcept
+{
+    CryptoPP::Base32HexEncoder Encoder;
+    std::string encoded_string = {};
+    Encoder.Put((CryptoPP::byte *)input_data.data(), input_data.size());
+    Encoder.MessageEnd();
+
+    CryptoPP::word64 size_word64 = Encoder.MaxRetrievable();
+    if (size_word64)
+    {
+        encoded_string.resize(size_word64);
+        Encoder.Get((CryptoPP::byte *)&encoded_string[0], encoded_string.size());
+    }
+
+    return encoded_string;
+}
