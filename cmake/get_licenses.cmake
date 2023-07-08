00f76d8b566707f654fdef0623fb42d4315f4db5
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-08 15:20:30 +0000

    adeed my define licenses to precompiling

---
+++ cmake/get_licenses.cmake
@@ -0,0 +1,36 @@
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in")
+    file(WRITE "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in" "")
+endif()
+
+file(APPEND "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in" "#ifndef TANJA84DK_CRYPT_LICENCES_MACRO_H\n")
+file(APPEND "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in" "#define TANJA84DK_CRYPT_LICENCES_MACRO_H\n\n")
+
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+    file(STRINGS ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE tanja84dk_crypt_LICENSE)
+    string(PREPEND tanja84dk_crypt_LICENSE "R\"(")
+    string(APPEND tanja84dk_crypt_LICENSE ")\"")
+    file(APPEND ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in "#define tanja84dk_crypt_LICENSE @tanja84dk_crypt_LICENSE@\n")
+else()
+    message("tanja84dk_crypt_LICENSE is missing")
+endif()
+
+if(EXISTS "${cryptopp-cmake_SOURCE_DIR}/LICENSE")
+    file(STRINGS ${cryptopp-cmake_SOURCE_DIR}/LICENSE cryptopp_cmake_LICENSE)
+    string(PREPEND cryptopp_cmake_LICENSE "R\"(")
+    string(APPEND cryptopp_cmake_LICENSE ")\"")
+    file(APPEND ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in "#define cryptopp_cmake_LICENSE @cryptopp_cmake_LICENSE@\n")
+else()
+    message("cryptopp_cmake_LICENSE is missing")
+endif()
+
+if(EXISTS "${cryptopp-cmake_BINARY_DIR}/cryptopp/License.txt")
+    file(STRINGS ${cryptopp-cmake_BINARY_DIR}/cryptopp/License.txt cryptopp_LICENSE)
+    string(PREPEND cryptopp_LICENSE "R\"(")
+    string(APPEND cryptopp_LICENSE ")\"")
+    file(APPEND ${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in "#define cryptopp_LICENSE @cryptopp_LICENSE@\n")
+else()
+    message("cryptopp_LICENSE is missing")
+endif()
+
+file(APPEND "${CMAKE_CURRENT_SOURCE_DIR}/${PROJECT_NAME}_licenses_macro.h.in"
+"\n#endif // TANJA84DK_CRYPT_LICENCES_MACRO_H\n")
