414b522806c870e7bbffd8f3fb481d3f479030b3
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-16 20:27:56 +0000

    changed from [0] to .data()

--- src/base64/base64.cpp
+++ src/base64/base64.cpp
@@ -15,7 +15,7 @@ std::string Tanja84dk::crypt::base64::decode(const std::string &input_data) noex
 
     if (size_word64 && size_word64 <= SIZE_MAX) {
         decoded_string.resize(size_word64);
-        Decoder.Get((CryptoPP::byte *)&decoded_string[0], decoded_string.size());
+        Decoder.Get((CryptoPP::byte *)decoded_string.data(), decoded_string.size());
     }
 
     return decoded_string;
@@ -24,7 +24,9 @@ std::string Tanja84dk::crypt::base64::decode(const std::string &input_data) noex
 std::string Tanja84dk::crypt::base64::encode(const std::string &input_data) noexcept {
     // Mayjor flaw in CryptoPP that you have to overule the default constructor
     // just for it to comply woth RFC 4648 https://datatracker.ietf.org/doc/html/rfc4648
+    // since they for some reason add a newline as default
     CryptoPP::Base64Encoder Encoder(NULL, false);
+
     std::string encoded_string = {};
 
     Encoder.Put((CryptoPP::byte *)input_data.data(), input_data.size());
@@ -34,7 +36,7 @@ std::string Tanja84dk::crypt::base64::encode(const std::string &input_data) noex
 
     if (size_word64 && size_word64 <= SIZE_MAX) {
         encoded_string.resize(size_word64);
-        Encoder.Get((CryptoPP::byte *)&encoded_string[0], encoded_string.size());
+        Encoder.Get((CryptoPP::byte *)encoded_string.data(), encoded_string.size());
     }
 
     return encoded_string;
