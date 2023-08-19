4662b4b29484513ba355698480d3bd8ddacde1c9
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-08-19 18:58:23 +0000

    Fixed cryptopp license and also deactivated building the tests

--- cmake/Findcryptopp-cmake.cmake
+++ cmake/Findcryptopp-cmake.cmake
@@ -3,4 +3,6 @@ CPMAddPackage(
     GIT_REPOSITORY  https://github.com/abdes/cryptopp-cmake
     VERSION         8.7.0
     GIT_TAG         CRYPTOPP_8_7_0
+    OPTIONS
+        "CRYPTOPP_BUILD_TESTING OFF"
 )
