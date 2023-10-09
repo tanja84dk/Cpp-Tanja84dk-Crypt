555729aba235aa456bc91c9e809233a849d72b51
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-10-09 19:14:09 +0000

    updated cryptopp to 8.9.0

--- cmake/Findcryptopp-cmake.cmake
+++ cmake/Findcryptopp-cmake.cmake
@@ -1,8 +1,8 @@
 CPMAddPackage(
     NAME            cryptopp-cmake
     GIT_REPOSITORY  https://github.com/abdes/cryptopp-cmake
-    VERSION         8.7.0
-    GIT_TAG         CRYPTOPP_8_7_0
+    VERSION         8.9.0
+    GIT_TAG         CRYPTOPP_8_9_0
     OPTIONS
         "CRYPTOPP_BUILD_TESTING OFF"
 )
