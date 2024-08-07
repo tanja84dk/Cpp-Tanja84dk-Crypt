be2790b21b5705851c0c937b9d6c5ff14f672c97
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2024-08-07 01:28:02 +0000

    added ReneNyffenegger base64

---
+++ cmake/Findrenenyffenegger_base64.cmake
@@ -0,0 +1,5 @@
+CPMAddPackage(
+    NAME            renenyffenegger_base64
+    GIT_REPOSITORY  https://gitea.home.tanja84.dk/Cpp/Cpp-Tanja84dk-ReneNyffenegger-base64
+    GIT_TAG         e7cb4bc6c5501f7870bf50e05cb5e3b86e2503cd
+)
