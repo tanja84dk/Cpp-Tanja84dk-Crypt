c8957d099a61cebcb64f4eb9be389a1f8cde1726
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2024-08-07 01:20:43 +0000

    Updated Catch2

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
     NAME            Catch2
     GIT_REPOSITORY  https://github.com/catchorg/Catch2
-    VERSION         3.4.0
-    GIT_TAG         v3.4.0
+    VERSION         3.6.0
+    GIT_TAG         v3.6.0
 )
