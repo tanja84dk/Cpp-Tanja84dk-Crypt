5c9660b46b9ce141f590ba0ff4fdb8a5ebb00412
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2024-08-13 22:06:31 +0000

    cleanup

--- include/Tanja84dk/crypt/ceasar/ceasar.h
+++ include/Tanja84dk/crypt/ceasar/ceasar.h
@@ -9,20 +9,19 @@ namespace crypt {
 
 class Caesar {
    public:
-    std::string encode(const int cipher_shift_key, const std::string &input_data) const noexcept;
-    std::string decode(const int cipher_shift_key, const std::string &input_data) const noexcept;
+    std::string encode(const int cipher_shift_key, const std::string &input_data_string) const noexcept;
+    std::string decode(const int cipher_shift_key, const std::string &input_data_string) const noexcept;
 
-    void decode_brute_force(const std::string &input_data);
+    void decode_brute_force(const std::string &input_data_string);
 
    private:
     std::string data_string = {};
 };
-namespace caesar {
+
 // std::string encode(const int cipher_shift_key, const std::string &input_data);
 // std::string decode(const int cipher_shift_key, const std::string &input_data);
 // void decode_brute_force(const std::string &input_data);
 
-}  // namespace caesar
 }  // namespace crypt
 }  // namespace Tanja84dk
 
